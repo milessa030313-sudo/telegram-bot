5fc2c027e9e3487de7df49bc68b81e551f1cd5d3
milessa030313-sudo <milessa030313@gmail.com>
2026-02-14 11:56:56 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+import asyncio
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+
+TOKEN = os.getenv("TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=[
+            [types.KeyboardButton(text="🏠 Аренда")],
+            [types.KeyboardButton(text="🏡 Продажа")],
+            [types.KeyboardButton(text="🛒 Товары")]
+        ],
+        resize_keyboard=True
+    )
+    await message.answer("🚀 Бот запущен!\nВыберите категорию:", reply_markup=keyboard)
+
+
+@dp.message()
+async def handler(message: types.Message):
+    await message.answer(f"Вы выбрали: {message.text}")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if name == "__main__":
+    asyncio.run(main())
