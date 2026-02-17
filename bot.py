f49545805f49e28f12a71f3f32c48cb6f7256901
milessa030313-sudo <milessa030313@gmail.com>
2026-02-17 22:30:58 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -249,3 +249,7 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     asyncio.create_task(monitor())
     await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+)
