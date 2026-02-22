d9c0c88f05224e91ccd959c9d8f8ce854f268f2a
milessa030313-sudo <milessa030313@gmail.com>
2026-02-22 12:43:36 +0500

    Create MonitorService for checking listings

---
+++ services/monitor_service.py
@@ -0,0 +1,8 @@
+class MonitorService:
+    def __init__(self):
+        # Initialize any necessary variables or configurations
+        pass
+
+    def check_listings(self):
+        # Logic for checking listings goes here
+        pass
