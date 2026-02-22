253ceb9ebf93f0992c79fe9794a26cae8ef96686
milessa030313-sudo <milessa030313@gmail.com>
2026-02-22 12:44:06 +0500

    Create utils/texts.py with bot messages and templates

---
+++ utils/texts.py
@@ -0,0 +1,16 @@
+# Bot Text Messages and Templates
+
+# Welcome message
+WELCOME_MESSAGE = 'Welcome to the Telegram Bot!'
+
+# Help message
+HELP_MESSAGE = 'Here are the commands you can use...'
+
+# Error message
+ERROR_MESSAGE = 'Oops! Something went wrong...'
+
+# Farewell message
+FAREWELL_MESSAGE = 'Thank you for using the bot! Goodbye!'
+
+# Template for user notifications
+USER_NOTIFICATION_TEMPLATE = 'User {username} has performed the action.'
