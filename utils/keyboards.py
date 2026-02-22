c192e3606d099262822a3a296639ce890912ca50
milessa030313-sudo <milessa030313@gmail.com>
2026-02-22 12:44:14 +0500

    Added keyboard layouts for the telegram bot.

---
+++ utils/keyboards.py
@@ -0,0 +1,19 @@
+# Keyboard Layouts for Telegram Bot
+
+# Layout for main keyboard
+main_keyboard = [
+    ['Start', 'Help'],
+    ['Settings']
+]
+
+# Layout for admin keyboard
+admin_keyboard = [
+    ['Manage Users', 'View Logs'],
+    ['Back to Main']
+]
+
+# Layout for user menu
+user_keyboard = [
+    ['Profile', 'Subscriptions'],
+    ['Feedback', 'Support']
+] 
